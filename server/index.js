a54501a90f6fae21839a60983ba7679db75d02cf
KurtShamrock <95069085+KurtShamrock@users.noreply.github.com>
2022-08-27 09:01:01 +0700

    mistake commit

--- server/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const connection = require("./db");
-const app = express();
-
-connection();
-
-const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
